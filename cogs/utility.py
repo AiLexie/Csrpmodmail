d1978de29e640a03cfb2616880cc020046a65051
SnPux <54323871+SnPux@users.noreply.github.com>
2021-10-05 19:42:52 -0400

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -319,7 +319,7 @@ class Utility(commands.Cog):
         embed.add_field(name="Uptime", value=self.bot.uptime)
         embed.add_field(name="Latency", value=f"{self.bot.latency * 1000:.2f} ms")
         embed.add_field(name="Version", value=f"`{self.bot.version}`")
-        embed.add_field(name="Authors", value="`kyb3r`, `Taki`, `fourjr`")
+        embed.add_field(name="Authors", value="`Lexie`, `Gang`, `Heck`")
         embed.add_field(name="Hosting Method", value=self.bot.hosting_method.name)
 
         changelog = await Changelog.from_url(self.bot)
